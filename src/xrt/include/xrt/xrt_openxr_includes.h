marioeatsdirt/Monado

xrt: Adds support for XR_META_body_tracking_full_body xrt-devices

src/xrt/include/xrt/xrt_openxr_includes.h
@@ -64,3 +64,7 @@ typedef __eglMustCastToProperFunctionPointerType (*PFNEGLGETPROCADDRESSPROC)(con
 #include "openxr/XR_MNDX_hydra.h"
 #include "openxr/XR_MNDX_system_buttons.h"
 #include "openxr/XR_MNDX_ball_on_a_stick_controller.h"
+
+#if defined(XRT_USE_META_EXT_HEADERS)
+#include "openxr/meta_body_tracking_full_body.h"
+#endif

src/xrt/state_trackers/oxr/oxr_extension_support.h
@@ -618,6 +618,18 @@
 #define OXR_EXTENSION_SUPPORT_FB_body_tracking(_)
 #endif
 
+
+/*
+ * XR_META_body_tracking_full_body
+ */
+#if defined(XR_META_body_tracking_full_body) && defined(XRT_FEATURE_OPENXR_BODY_TRACKING_FULL_BODY_META)
+#define OXR_HAVE_META_body_tracking_full_body
+#define OXR_EXTENSION_SUPPORT_META_body_tracking_full_body(_)                                                          \
+	_(META_body_tracking_full_body, META_BODY_TRACKING_FULL_BODY)
+#else
+#define OXR_EXTENSION_SUPPORT_META_body_tracking_full_body(_)
+#endif
+
 // end of GENERATED per-extension defines - do not modify - used by scripts
 
 /*!
@@ -694,5 +706,6 @@
     OXR_EXTENSION_SUPPORT_MNDX_hydra(_) \
     OXR_EXTENSION_SUPPORT_MNDX_system_buttons(_) \
     OXR_EXTENSION_SUPPORT_HTC_facial_tracking(_) \
-    OXR_EXTENSION_SUPPORT_FB_body_tracking(_)
+    OXR_EXTENSION_SUPPORT_FB_body_tracking(_) \
+    OXR_EXTENSION_SUPPORT_META_body_tracking_full_body(_)
 // clang-format on

leoscholl/aolab-monado

src/xrt/include/xrt/xrt_openxr_includes.h
@@ -67,3 +67,7 @@ typedef __eglMustCastToProperFunctionPointerType (*PFNEGLGETPROCADDRESSPROC)(con
 #include "openxr/XR_MNDX_xdev_space.h"
 #include "openxr/XR_MNDX_system_buttons.h"
 #include "openxr/XR_MNDX_ball_on_a_stick_controller.h"
+
+#if defined(XRT_USE_META_EXT_HEADERS)
+#include "meta_openxr_preview/meta_body_tracking_full_body.h"
+#endif

src/xrt/state_trackers/oxr/oxr_extension_support.h
@@ -724,6 +724,18 @@
 #define OXR_EXTENSION_SUPPORT_MNDX_xdev_space(_)
 #endif
 
+
+/*
+ * XR_META_body_tracking_full_body
+ */
+#if defined(XR_META_body_tracking_full_body) && defined(XRT_FEATURE_OPENXR_BODY_TRACKING_FULL_BODY_META)
+#define OXR_HAVE_META_body_tracking_full_body
+#define OXR_EXTENSION_SUPPORT_META_body_tracking_full_body(_)                                                          \
+	_(META_body_tracking_full_body, META_BODY_TRACKING_FULL_BODY)
+#else
+#define OXR_EXTENSION_SUPPORT_META_body_tracking_full_body(_)
+#endif
+
 // end of GENERATED per-extension defines - do not modify - used by scripts
 
 /*!
@@ -793,6 +805,7 @@
     OXR_EXTENSION_SUPPORT_FB_touch_controller_pro(_) \
     OXR_EXTENSION_SUPPORT_FB_touch_controller_proximity(_) \
     OXR_EXTENSION_SUPPORT_HTC_facial_tracking(_) \
+    OXR_EXTENSION_SUPPORT_META_body_tracking_full_body(_) \
     OXR_EXTENSION_SUPPORT_META_touch_controller_plus(_) \
     OXR_EXTENSION_SUPPORT_ML_ml2_controller_interaction(_) \
     OXR_EXTENSION_SUPPORT_MND_headless(_) \