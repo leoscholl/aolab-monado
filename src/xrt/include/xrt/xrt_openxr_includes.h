marioeatsdirt/Monado

xrt: Adds support for XR_META_body_tracking_fidelity

src/xrt/include/xrt/xrt_openxr_includes.h
@@ -67,4 +67,5 @@ typedef __eglMustCastToProperFunctionPointerType (*PFNEGLGETPROCADDRESSPROC)(con
 
 #if defined(XRT_USE_META_EXT_HEADERS)
 #include "openxr/meta_body_tracking_full_body.h"
+#include "openxr/meta_body_tracking_fidelity.h"
 #endif

leoscholl/aolab-monado

src/xrt/include/xrt/xrt_openxr_includes.h
@@ -70,4 +70,5 @@ typedef __eglMustCastToProperFunctionPointerType (*PFNEGLGETPROCADDRESSPROC)(con
 
 #if defined(XRT_USE_META_EXT_HEADERS)
 #include "meta_openxr_preview/meta_body_tracking_full_body.h"
+#include "meta_openxr_preview/meta_body_tracking_fidelity.h"
 #endif